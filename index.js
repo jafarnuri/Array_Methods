8175209ebb80b1556c92f09622a12f77284469d6
jafarnuri <jafarnuri1994@gmail.com>
2024-09-01 23:41:33 +0400

    array methods

---
+++ index.js
@@ -0,0 +1,40 @@
+// let fillter = [15, -5, 7, -99, -2, 15, 55];
+// let filters = [];
+// function Showfilter() {
+//   for (let i = 0; i <= fillter.length; i++) {
+//     if (fillter[i] > 10 && fillter[i] < 50) {
+//       filters.push(fillter[i]);
+//     }
+//   }
+
+//   console.log(filters);
+// }
+// Showfilter();
+
+// let every = [20, 40, 33, 60, 25];
+// let num = +prompt("bir eded girin...");
+// function Showevery(num) {
+//   for (let i = 0; i <= every.length; i++) {
+//     if (num > every[i]) {
+//       console.log(false);
+//     } else {
+//       console.log(true);
+//     }
+//   }
+//   console.log(every);
+// }
+// Showevery(num);
+
+// let some = [20, 40, 33, 60, 25];
+// let num = +prompt("bir eded girin...");
+// function Showsome(num) {
+//   for (let i = 0; i <= some.length; i++) {
+//     if (some[i] % num === 0) {
+//       console.log(true);
+//     } else {
+//       console.log(false);
+//     }
+//   }
+//   console.log(some);
+// }
+// Showsome(num);
